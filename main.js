4fa41ddc476ce688fa5f30bd8abb7ac15fca0bfe
GaJae <bangjoshua@naver.com>
2022-03-26 09:18:36 +0900

    함수화를 통한 코드 정리 関数化によるコードの整理

--- main.js
+++ main.js
@@ -1,55 +1,123 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
-var app = http.createServer(function(request,response){
-    var _url = request.url;
-    var queryData = url.parse(_url,true).query;
-    //console.log(queryData.id);
-    var title = queryData.id;
-    //if(request.url == '/'){
-      if(_url == '/'){
-        //_url = '/index.html';
-        title = 'Welcome';
+const { strictEqual } = require('assert');
+var app = http.createServer(function (request, response) {
+  var _url = request.url;
+  var queryData = url.parse(_url, true).query;
+  //console.log(queryData.id);
+
+  var pathname = url.parse(_url, true).pathname;
+
+  function templateHTML(title, list, body) {
+    return `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            ${list}
+           ${body}
+          </body>
+          </html>
+          `;
+
+  }
+  function templateList(filelist) {
+    var list = '<ol>';
+
+    for (i of filelist) {
+      list += `<li><a href="/?id=${i}">${i}</a></li>`;
     }
-    //if(request.url == '/favicon.ico'){
-      if(_url == '/favicon.ico'){
-        // response.writeHead(404);
-        // response.end();
-        // return;
-        return response.writeHead(404);
+
+    list += '</ol>';
+    return list;
+  }
+
+  //if(request.url == '/'){
+  /* if(_url == '/'){
+    //_url = '/index.html';
+    title = 'Welcome';
+  }
+//if(request.url == '/favicon.ico'){
+  if(_url == '/favicon.ico'){
+    // response.writeHead(404);
+    // response.end();
+    // return;
+    return response.writeHead(404);
+} */
+  //response.writeHead(200);
+  //console.log(__dirname + _url);
+  //response.end('egoing' + url);
+  //response.end(fs.readFileSync(__dirname + _url));
+
+  console.log(url.parse(_url, true).pathname);
+
+  if (pathname === '/') {
+    if (queryData.id === undefined) {
+
+      //fs.readFile(`data/${queryData.id}`,'utf-8',function(err,description){
+
+      fs.readdir('./data', function (error, filelist) {
+        console.log(filelist);
+        var title = 'Welcome';
+        var description = 'Hello, Node.js';
+        /*
+         var list =`      <ol>
+           <li><a href="/?id=HTML">HTML</a></li>
+           <li><a href="/?id=CSS">CSS</a></li>
+           <li><a href="/?id=JavaScript">JavaScript</a></li>
+         </ol>`;
+         */
+        var list = templateList(filelist);
+        /* '<ol>';
+
+      for (i of filelist) {
+        list += `<li><a href="/?id=${i}">${i}</a></li>`;
+      }
+
+      list += '</ol>'; */
+
+        var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`);
+        response.writeHead(200);
+        response.end(template);
+      });
+
+      //});
+    } else {
+      fs.readdir('./data', function (error, filelist) {
+        //console.log(filelist);
+
+        /*
+          var list =`      <ol>
+            <li><a href="/?id=HTML">HTML</a></li>
+            <li><a href="/?id=CSS">CSS</a></li>
+            <li><a href="/?id=JavaScript">JavaScript</a></li>
+          </ol>`;
+          */
+        var list = templateList(filelist);
+
+        fs.readFile(`data/${queryData.id}`, 'utf-8', function (err, description) {
+          var title = queryData.id;
+          var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`);
+          response.writeHead(200);
+          response.end(template);
+        });
+      });
     }
-    response.writeHead(200);
-    //console.log(__dirname + _url);
-    //response.end('egoing' + url);
-    //response.end(fs.readFileSync(__dirname + _url));
-    fs.readFile(`data/${queryData.id}`,'utf-8',function(err,description){
-    
-      var template=`
-      <!doctype html>
-      <html>
-      <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1><a href="/">WEB</a></h1>
-        <ol>
-          <li><a href="/?id=HTML">HTML</a></li>
-          <li><a href="/?id=CSS">CSS</a></li>
-          <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ol>
-        <h2>${title}</h2>
-        <p>
-        ${description}
-        </p>
-      </body>
-      </html>
-      `
-      response.end(template);
-    });
-    
-    //response.end(queryData.id);
-   
-     
+
+  } else {
+    response.writeHead(404);
+    response.end('Not found');
+  }
+
+
+
+  //response.end(queryData.id);
+
+
 });
 app.listen(3000);
