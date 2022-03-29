c63bdface9f85c2b0dbed8744a44756aed54b05c
GaJae <bangjoshua@naver.com>
2022-03-29 20:17:56 +0900

    update function complete

--- main.js
+++ main.js
@@ -74,15 +74,6 @@ var app = http.createServer(function (request, response) {
       //});
     } else {
       fs.readdir('./data', function (error, filelist) {
-        //console.log(filelist);
-
-        /*
-          var list =`      <ol>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-          </ol>`;
-          */
         var list = templateList(filelist);
 
         fs.readFile(`data/${queryData.id}`, 'utf-8', function (err, description) {
@@ -135,8 +126,58 @@ var app = http.createServer(function (request, response) {
           response.end();
         });
     });
+  }
+  else if (pathname === '/update') {
+    fs.readdir('./data', function (error, filelist) {
+      var list = templateList(filelist);
 
+      fs.readFile(`data/${queryData.id}`, 'utf-8', function (err, description) {
+        var title = queryData.id;
+        var template = templateHTML(title, list,
+          `
+           <form action="/update_process" method="post">
+           <input type="hidden" name="id" value ="${title}">
+            <p><input type="text" name="title" placeholder="title" value ="${title}"></p>
+            <p>
+              <textarea name="description" placeholder="description">${description}</textarea>
+            </p>
+            <p>
+              <input type="submit">
+            </p>
+          </form>
+          
+          `,
+          `<a href="/create">ceate</a> <a href="/update?id=${title}">update</a>`
+        );
+        response.writeHead(200);
+        response.end(template);
+      });
+    });
+  }
+  else if (pathname === '/update_process') {
+    var body = '';
+    request.on('data', function (data) {//데이터 요청이 발생햇을때마다 실행됨
+      body += data;
+    });
+    request.on('end', function () {//정보수신이 끝낫을때
+      var post = qs.parse(body);
+      var id = post.id;
+      var title = post.title;
+      var description = post.description;
 
+      fs.rename(`data/${id}`, `data/${title}`, function (error) {
+        fs.writeFile(`data/${title}`, description, 'utf8',
+          function (err) {
+            response.writeHead(302, { Location: `/?id=${title}` });
+            response.end();
+          });
+      });
+
+      //console.log(post);
+      /*
+      
+      */
+    });
   }
   else {
     response.writeHead(404);
